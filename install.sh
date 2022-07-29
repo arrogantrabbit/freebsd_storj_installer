0cdc3efbad6c1e1719ee77ca2b4fdde4e605c7ef
Arrogant Rabbit <chipmunk@arrogantrabbit.com>
2022-07-29 02:02:13 -0700

    Implemented storagenode-updater for FreeBSD as the official one does not know how to restart freebsd service

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ IDENTITY_AUTH_TOKEN="CHANGE_ME"
 # External address and port, setup port forwarding as needed
 CONTACT_EXTERNAL_ADDRESS="example.com:28968"
 # operator email address
-OPERATOR_EMAIL="me@saspus.com"
+OPERATOR_EMAIL="user@example.com"
 # Wallet
 OPERATOR_WALLET="0x0eC33087AFfed2924aB01579abc2c471BC6Da13C"
 OPERATOR_WALLET_FEATURES=""
@@ -25,6 +25,13 @@ if [ "$IDENTITY_AUTH_TOKEN" == "CHANGE_ME" ]; then
 	exit 1
 fi
 
+# Prerequisities
+
+pkg install -y jq curl
+
+# NOTE on storagenode-updater. As of today, storagenode updater does not know how to restart the service on freebsd. While it successfuly updates the executable it continues running the old one.
+# Until the situation changes we include a simple shell script instead of storage node updater that ignores input parameters and simply does the job.
+
 
 HOME="/root"
 
@@ -35,13 +42,14 @@ STORAGNODE_IDENTITY_DIR="${HOME}/.local/share/storj/identity/storagenode"
 
 STORAGENODE_URL="https://github.com/storj/storj/releases/download/${VERSION}/storagenode_freebsd_amd64.zip"
 
-STORAGENODE_UPDATER_URL="https://github.com/storj/storj/releases/download/${VERSION}/storagenode-updater_freebsd_amd64.zip"
+#STORAGENODE_UPDATER_URL="https://github.com/storj/storj/releases/download/${VERSION}/storagenode-updater_freebsd_amd64.zip"
 IDENTITY_URL="https://github.com/storj/storj/releases/download/${VERSION}/identity_freebsd_amd64.zip"
 
 mkdir -p /tmp/${VERSION}
-STORAGENODE_ZIP=/tmp/${VERSION}/$(basename ${STORAGENODE_URL})
-STORAGENODE_UPDATER_ZIP=/tmp/${VERSION}/$(basename ${STORAGENODE_UPDATER_URL})
+
 IDENTITY_ZIP=/tmp/${VERSION}/$(basename ${IDENTITY_URL})
+STORAGENODE_ZIP=/tmp/${VERSION}/$(basename ${STORAGENODE_URL})
+#STORAGENODE_UPDATER_ZIP=/tmp/${VERSION}/$(basename ${STORAGENODE_UPDATER_URL})
 
 TARGET_BIN_DIR="/usr/local/bin"
 
@@ -63,10 +71,9 @@ fetch() {
 
 echo "Fetching executables from the internet"
 
-fetch "${STORAGENODE_URL}" "${STORAGENODE_ZIP}"
-fetch "${STORAGENODE_UPDATER_URL}" "${STORAGENODE_UPDATER_ZIP}"
 fetch "${IDENTITY_URL}"    "${IDENTITY_ZIP}"
-
+fetch "${STORAGENODE_URL}" "${STORAGENODE_ZIP}"
+#fetch "${STORAGENODE_UPDATER_URL}" "${STORAGENODE_UPDATER_ZIP}"
 
 echo "Stopping existing services"
 
@@ -74,11 +81,14 @@ service storj stop 2>/dev/null >/dev/null
 service storjupd stop 2>/dev/null >/dev/null
 
 
+echo "Copying rc scripts and replacement updater"
+cp -rv root/ /
+
 echo "Extracting binaries to ${TARGET_BIN_DIR}"
 
 unzip -d "${TARGET_BIN_DIR}" -o "${IDENTITY_ZIP}" 
 unzip -d "${TARGET_BIN_DIR}" -o "${STORAGENODE_ZIP}" 
-unzip -d "${TARGET_BIN_DIR}" -o "${STORAGENODE_UPDATER_ZIP}" 
+#unzip -d "${TARGET_BIN_DIR}" -o "${STORAGENODE_UPDATER_ZIP}" 
 
 
 if [ ! -f "${STORAGNODE_IDENTITY_DIR}/identity.cert" ]; then
@@ -110,9 +120,6 @@ if [ ! -f "${HOME}/.local/share/storj/storagenode/config.yaml" ]; then
 fi
 
 
-echo "Copying rc scripts"
-cp -rv root/ /
-
 
 echo "Configuring netwait to wait for ${NETWAIT_IP}"
 sysrc netwait_ip="${NETWAIT_IP}"
@@ -133,6 +140,6 @@ service netwait enable
 
 echo "Starting services"
 
-service storj start
-service storjupd start
+#service storj start
+#service storjupd start
 service newsyslog start
